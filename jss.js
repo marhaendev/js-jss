531585bfaff55551ddcb5f4f4b51e20cfbd8eee2
marhaendev <marhaendev@gmail.com>
2024-06-16 23:35:57 +0700

    init

--- jss.js
+++ jss.js
@@ -1,7 +1,7 @@
 
 
 // ================ CLASSES
-// ac-
+// =========== ac-
 
 document.addEventListener("DOMContentLoaded", function() {
     // Select all elements with class names starting with 'ac-'
@@ -55,3 +55,50 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     });
   });
+
+
+// =========== clav.js
+// class view adjustment
+
+  function applyResponsiveClasses() {
+    const elements = document.querySelectorAll('[class*="sm-"], [class*="st-"], [class*="sd-"], [class*="std-"], [class*="smt-"], [class*="smd-"]');
+    const width = window.innerWidth;
+
+    elements.forEach(element => {
+        const classList = element.className.split(' ');
+
+        // Remove all specific classes
+        classList.forEach(cls => {
+            if (cls.startsWith('sm-') || cls.startsWith('st-') || cls.startsWith('sd-') || cls.startsWith('std-') || cls.startsWith('smt-') || cls.startsWith('smd-')) {
+                element.classList.remove(cls.replace('sm-', '').replace('st-', '').replace('sd-', '').replace('std-', '').replace('smt-', '').replace('smd-', ''));
+            }
+        });
+
+        // Add specific classes based on the width
+        if (width < 768) {
+            classList.forEach(cls => {
+                if (cls.startsWith('sm-') || cls.startsWith('smt-') || cls.startsWith('smd-')) {
+                    element.classList.add(cls.replace('sm-', '').replace('smt-', '').replace('smd-', ''));
+                }
+            });
+        } else if (width >= 768 && width < 1024) {
+            classList.forEach(cls => {
+                if (cls.startsWith('st-') || cls.startsWith('std-') || cls.startsWith('smt-')) {
+                    element.classList.add(cls.replace('st-', '').replace('std-', '').replace('smt-', ''));
+                }
+            });
+        } else if (width >= 1024) {
+            classList.forEach(cls => {
+                if (cls.startsWith('sd-') || cls.startsWith('std-') || cls.startsWith('smd-')) {
+                    element.classList.add(cls.replace('sd-', '').replace('std-', '').replace('smd-', ''));
+                }
+            });
+        }
+    });
+}
+
+// Apply classes on load
+applyResponsiveClasses();
+
+// Apply classes on resize
+window.addEventListener('resize', applyResponsiveClasses);
